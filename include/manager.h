fbf50ac8fab9f0bc569098cf4a8d6138b0d9c670
Nhutnguyen395 <nhutnguyen395@gmail.com>
2025-05-04 17:26:37 -0700

    done

--- include/manager.h
+++ include/manager.h
@@ -7,6 +7,19 @@
 #include <string.h>
 #include <math.h>
 
+#define PAGE_SIZE 512
+#define FRAME_SIZE 1024
+#define PHYSICAL_MEMORY_SIZE
 
+extern int physical_memory[PHYSICAL_MEMORY_SIZE];
+extern int disk[FRAME_SIZE][PAGE_SIZE];
+extern int free_frames[FRAME_SIZE];
+extern int free_frames_count;
+
+void init(const char* init_file);
+int translate_address(uint32_t va);
+void process_addresses(const char* va_file, const char* output_file);
+int allocate_free_frame();
+void read_block(int block, int frame_start);
 
 #endif
